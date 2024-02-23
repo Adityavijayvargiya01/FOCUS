1d6ad5966882df29575a583db9fb82590b338a5d
Adityavijayvargiya01 <adityavijayvargiya2022@gmail.com>
2024-02-24 02:25:23 +0530

    Update title and description in metadata, add navbar and toaster components, and add dependencies

---
+++ Resources/components/navbar.jsx
@@ -0,0 +1,33 @@
+import React from 'react'
+import { CheckCircle, Circle } from "@phosphor-icons/react/dist/ssr";
+import { Toggle } from './ui/toggle';
+import { useToast } from "@/components/ui/use-toast"
+
+
+export default function navbar() {
+  return (
+    <main>
+      <div className='flex flex-row items-center justify-between h-24 w-full p-5'>
+
+
+      <div className='flex items-center'>
+      <h1 className="scroll-m-20 text-4xl font-bold tracking-light lg:text-5xl px-3 text-muted-foreground">
+      Focus
+      </h1>
+      <Circle size={42} color='#5ec26a' className='mt-2' />
+      </div>
+
+      <div>
+        <Toggle>
+          FOCUS MODE
+        </Toggle>
+      </div>
+
+      </div>
+
+
+
+
+    </main> 
+  )
+}
